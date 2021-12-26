4b5a0344a515906e7be4080791db3f0b2d3e240f
S1nglz <Boewer.gil@gmail.com>
2021-12-26 15:41:07 +0100

    basic statemachine + window feed

---
+++ Bots/MoneyFarmer/Project/src/botstatemachine.py
@@ -0,0 +1,21 @@
+class BotStateMachine:
+    def __init__(self):
+        self.state = None
+
+    def setState(self, state):
+        if(not self.state == None):
+            self.state.exit()
+        self.state = state
+        state.enter()
+
+    def run(self):
+        self.state.run()
+
+class State:
+    def __init__(self, bot):
+        self.bot = bot
+        self.windowHandle = bot.windowHandle
+
+    def enter(self): pass
+    def run(self): pass
+    def exit(self): pass
